0e4879b04da892c2dacada00d3a1c6c9334e38b2
Bryce <bryce.bolin@yahoo.com>
2025-11-03 16:36:37 -0600

    add ui to game

--- js.js
+++ js.js
@@ -1,11 +1,47 @@
-function getComputerChoice () {
-     let number = Math.floor(Math.random()*3) + 1;
-    
-     if (number === 1) {
-        return "Rock"
-     } else if (number === 2){
-        return "Paper"
-     } else {
-        return "Scissors"
-     };
+
+
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+
+rockButton.addEventListener('click', function () {
+   const hChoice = "rock";
+   console.log(hChoice);
+   playRound(hChoice);
+});
+
+paperButton.addEventListener('click', function() {
+   const hChoice = "paper";
+   console.log(hChoice);
+   playRound(hChoice);
+});
+
+scissorsButton.addEventListener('click', function() {
+   const hChoice = "scissors";
+   console.log(hChoice);
+   playRound(hChoice);
+})
+
+
+function playRound(hChoice) {
+   let choices = ['rock', 'paper', 'scissors']
+   let cChoice = choices[Math.floor(Math.random() * choices.length)];
+   
+   if (hChoice === cChoice) {
+      console.log("It's a tie!");
+   } else if (
+      (hChoice === "rock" && cChoice === "scissors") ||
+      (hChoice === "paper" && cChoice === "rock") ||
+      (hChoice === "scissors" && cChoice === "paper")
+   ){
+      console.log("You win!")
+   } else {
+      console.log("Computer Wins!");
+   }
 }
+
+
+
+
+
+
